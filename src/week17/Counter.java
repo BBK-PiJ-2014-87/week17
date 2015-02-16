249239f77c7499cb67f4558f6b5023d919aabe7b
BBK-PiJ-2014-87 <ivanovs223@gmail.com>
2015-02-16 18:57:42 +0000

    Added syncronized object

--- src/week17/Counter.java
+++ src/week17/Counter.java
@@ -2,8 +2,14 @@ package week17;
 
 public class Counter {
 	private int n = 0;
+	//to get correct output form multiple threads
+	//we need to create an object for lock acquire
+	private Object obj = new Object();
 	public void increase(){
-		n++;
+		synchronized (obj){
+			n++;
+		}
+		
 	}
 	public int getCount(){
 		return n;
